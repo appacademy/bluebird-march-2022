8d37310ae00773e39bb8ad4bee492cd43acc313e
aycelacap <[raycel5lacap@gmail.com]>
2022-04-29 13:22:05 -0400

    Add active record

--- app/models/chirp.rb
+++ app/models/chirp.rb
@@ -32,4 +32,79 @@ class Chirp < ApplicationRecord
     has_many :likers,
         through: :likes,
         source: :liker
+
+     # Includes 
+
+  def self.see_chirp_authors_n_plus_one
+      # the "+1"
+      chirps = Chirp.all
+
+      # the "N"
+      chirps.each do |chirp|
+          puts chirp.author.username
+      end
+
+  end
+
+  def self.see_chirps_optimized
+      # pre-fetches data
+      chirps = Chirp.includes(:author).all
+
+      chirps.each do |chirp| 
+      # uses pre-fetched data 
+          puts chirp.author.username
+      end
+  end
+
+  # Joins #
+
+  def self.see_chirp_num_likes_n_plus_one
+      chirps = Chirp.all
+
+      chirps.each do |chirp|
+          puts chirp.likes.length
+      end
+  end
+
+  def self.see_chirp_num_likes_optimized
+      chirps_with_likes = Chirp
+          .select("chirps.*, COUNT(*) AS num_likes")
+          .joins(:likes)
+          .group(:id)
+  
+      chirps_with_likes.each do |chirp|
+          puts chirp.num_likes
+      end
+  end
+
 end
+
+  # demo 2
+
+  # Find all chirps for a particular user
+  # Chirp.joins(:author).where(users: {username: "instructors_rock"})
+  # SQL equiv = WHERE users.username = ""
+
+  # joins always ALWAYS ALWAYS takes in an association
+
+  # Find all chirps liked by people politically affiliated with JavaScript
+  # Chirp.joins(:likers).where("users.political_affiliation = (?)", "JavaScript")
+
+  #Get only the unique values from the previous query
+  # ------------ DISTINCT ---------------
+  # Chirp.joins(:likers).where("users.political_affiliation = (?)", "JavaScript").distinct
+  # also add how we can use group
+#  --------------- GROUP ----------------
+  # Chirp.joins(:likers).group(:id).where("users.political_affiliation = (?)", "JavaScript")
+
+  #Find all chirps with no likes
+  # Chirp.left_outer_joins(:likes).where(likes: {id: nil})
+
+  #Find how many likes each chirp has
+  # Chirp.select(:id, :body, "COUNT(*) AS num_likes").joins(:likes).group(:id)
+
+  #Find chirps with at least 3 likes
+  # Chirp.joins(:likes).group(:id).having("COUNT(*) >= ?", 3).pluck(:body)
+  # Chirp.joins(:likes).group(:id).having("COUNT(*) >= ?", 3).select(:body)
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,42 @@ class User < ApplicationRecord
     has_many :liked_chirps,
         through: :likes,
         source: :chirp
+
+    #Get first user record, use first
+    # User.first
+
+    #Get last user record, use last
+    # User.last
+
+    #Find a user that exists by id, use find
+    # User.find(12)
+
+    #Find a user that doesn't exist by id, use find
+    #  User.find(37)
+
+    #Find a user by username, use find_by
+    #  User.find_by(username: “alvin_aka_mr_structy”)
+    
+    #Find a user by username that does not exist, use find_by
+    # User.find_by(username: "alviny")
+
+
+    #Find all users between the ages of 10 and 20 inclusive. Show their username, and political affiliation.
+    # User.where(age: 10..20).select(:username, :political_affiliation)
+
+    #Find all users not younger than the age of 11. Use `where.not`
+    # User.where.not("age <= ?", 11)
+
+    #Find all political_affiliations of our users
+    # User.select(:political_affiliation).group(:political_affilication)
+
+    # User.select(:political_affiliation).distinct
+
+    # BONUS
+    #Find all users who have a political affiliation in this list and order by ascending
+    # political_affiliations = ["Ruby", "JavaScript"]
+
+    # User.where(political_affiliation: ["Ruby", "JavaScript"]).order(username: :asc)
+
+
 end
